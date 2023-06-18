42edcebdef483e1580f2c2238e4ac62849980387
Raihan Rifandi <112451785+raihanrifandi@users.noreply.github.com>
2023-06-18 21:00:55 +0700

    next and previous added

--- main.py
+++ main.py
@@ -2,22 +2,20 @@ import random
 import os
 
 class Lagu:
-    def __init__(self, genre, judul, artis, durasi):
-        self.genre = genre # kategori 1
+    def __init__(self, genre, judul, artis, durasi, priority):
+        self.genre = genre 
         self.judul = judul 
         self.artis = artis 
         self.durasi = durasi
-        self.priority = random.random()  
-
-    def __lt__(self, other):
-        return self.priority < other.priority
+        self.priority = priority
 
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+        self.prev = None
 
-class LinkedList:
+class DoublyLinkedList:
     def __init__(self):
         self.head = None
 
@@ -33,6 +31,7 @@ class LinkedList:
             while current.next is not None:
                 current = current.next
             current.next = new_node
+            new_node.prev = current
 
     def delete_at_beginning(self):
         if self.is_empty():
@@ -40,46 +39,69 @@ class LinkedList:
         else:
             temp = self.head
             self.head = self.head.next
+            if self.head is not None:
+                self.head.prev = None
             temp.next = None
             return temp.data
-    
+
+    def delete_at_end(self):
+        if self.is_empty():
+            return None
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            if current.prev is not None:
+                current.prev.next = None
+            else:
+                self.head = None
+            current.prev = None
+            return current.data
+
 class PlaylistPlayer:
     def __init__(self):
-        self.queue = LinkedList()
-        self.stack = LinkedList()
-        self.deque = LinkedList()
+        self.queue = DoublyLinkedList()
+        self.stack = DoublyLinkedList()
+        self.deque = DoublyLinkedList()
         self.genre_dict = {}
-        self.genre_bst = None
+        self.current_song = None
         
-    def add_song(self, genre, lagu, artis, durasi):
-        lagu_baru = Lagu(genre, lagu, artis, durasi)
+    def add_song(self, genre, judul, artis, durasi):
+        priority = self._get_next_priority()
+        lagu_baru = Lagu(genre, judul, artis, durasi, priority)
         self.queue.insert_at_end(lagu_baru)
         self.stack.insert_at_end(lagu_baru)
         self.deque.insert_at_end(lagu_baru)
         self._update_genre_dict(genre, lagu_baru)
-        self._update_genre_bst(genre)
+
+    def _get_next_priority(self):
+        current = self.stack.head
+        max_priority = 0
+        while current:
+            if current.data.priority > max_priority:
+                max_priority = current.data.priority
+            current = current.next
+        return max_priority + 1
 
     def shuffle_songs(self):
-        # Convert linked list to list for shuffling
         songs = []
         current = self.queue.head
         while current:
             songs.append(current.data)
             current = current.next
         random.shuffle(songs)
-        # Rebuild the queue with shuffled songs
-        self.queue = LinkedList()
-        for song in songs:
-            self.queue.insert_at_end(song)
-
-    def play_songs(self):
         current = self.queue.head
-        while current:
-            print("Now playing:", current.data.judul)
+        for song in songs:
+            current.data = song
             current = current.next
 
+    def play_songs(self):
+        if not self.queue.is_empty():
+            print("Now playing:", self.queue.head.data.judul)
+        else:
+            print("No songs in the playlist.")
+
     def sort_by_newest(self):
-        # Convert linked list to list for sorting
         songs = []
         current = self.stack.head
         while current:
@@ -87,103 +109,78 @@ class PlaylistPlayer:
             current = current.next
         songs.sort(key=lambda x: x.priority, reverse=True)
         # Rebuild the stack with sorted songs
-        self.stack = LinkedList()
+        self.stack = DoublyLinkedList()
         for song in songs:
             self.stack.insert_at_end(song)
 
     def sort_by_oldest(self):
-        # Convert linked list to list for sorting
         songs = []
         current = self.stack.head
         while current:
             songs.append(current.data)
             current = current.next
         songs.sort(key=lambda x: x.priority)
-        # Rebuild the stack with sorted songs
-        self.stack = LinkedList()
+        self.stack = DoublyLinkedList()
         for song in songs:
             self.stack.insert_at_end(song)
 
-    def play_next_song(self):
-        if not self.deque.is_empty():
-            song = self.deque.delete_at_beginning()
-            self.deque.insert_at_end(song)
-            print("Now playing:", song.judul)
+    def next_song(self):
+        if self.current_song is None:
+            self.current_song = self.queue.head
         else:
-            print("No next song in the queue.")
-
-    def play_previous_song(self):
-        if not self.deque.is_empty():
-            current = self.deque.head
-            prev = None
-            while current.next:
-                prev = current
-                current = current.next
-            if prev:
-                prev.next = None
-                self.deque.head = current
-                current.next = self.deque.head
-                print("Now playing:", current.data.judul)
+            if self.current_song.next is None:
+                self.current_song = self.queue.head
             else:
-                print("No previous song in the queue.")
-                
+                self.current_song = self.current_song.next
+
+        if self.current_song is not None:
+            print("Now playing:", self.current_song.data.judul)
+        else:
+            print("No more songs in the playlist.")
+    
+    def previous_song(self):
+        if self.current_song is None:
+            print("No previous song in the playlist.")
+        else:
+            self.current_song = self.current_song.prev
+
+            if self.current_song is not None:
+                print("Now playing:", self.current_song.data.judul)
+            else:
+                print("No previous song in the playlist.")
+        
     def display_stack(self):
         current = self.stack.head
         while current:
-            print("Genre :", current.data.genre)
-            print("Judul :", current.data.judul)
-            print("Artis :", current.data.artis)
+            print("Genre:", current.data.genre)
+            print("Judul:", current.data.judul)
+            print("Artis:", current.data.artis)
             print("Durasi:", current.data.durasi, "menit")
-            print("="*25)
+            print("=" * 25)
             current = current.next
-            
+
     def _update_genre_dict(self, genre, song):
         if genre not in self.genre_dict:
-            self.genre_dict[genre] = LinkedList()
+            self.genre_dict[genre] = DoublyLinkedList()
         self.genre_dict[genre].insert_at_end(song)
-    
-    def _update_genre_bst(self, genre):
-        if self.genre_bst is None:
-            self.genre_bst = BinarySearchTree(genre)
-        else:
-            self.genre_bst.insert(genre)
-    
+
     def search_by_genre(self, genre):
-        genre = genre.lower()
         if genre in self.genre_dict:
             current = self.genre_dict[genre].head
             while current:
-                print("Genre :", current.data.genre)
-                print("Judul :", current.data.judul)
-                print("Artis :", current.data.artis)
+                print("Genre:", current.data.genre)
+                print("Judul:", current.data.judul)
+                print("Artis:", current.data.artis)
                 print("Durasi:", current.data.durasi, "menit")
-                print("="*25)
+                print("=" * 25)
                 current = current.next
         else:
             print("No songs found in the genre:", genre)
 
-class BinarySearchTree:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-
-    def insert(self, data):
-        if data < self.data:
-            if self.left is None:
-                self.left = BinarySearchTree(data)
-            else:
-                self.left.insert(data)
-        else:
-            if self.right is None:
-                self.right = BinarySearchTree(data)
-            else:
-                self.right.insert(data)
-            
 p = PlaylistPlayer()
 os.system('cls')
-file = open("header.txt", "r")
-print(file.read())
+# file = open("header.txt", "r")
+# print(file.read())
 
 while True:
     print("Menu Utama")
@@ -196,30 +193,45 @@ while True:
     pilihan = input("Masukkan pilihan menu: ")
 
     if pilihan == "1":
-        os.system('cls')
-        p.play_songs
-    
-    elif pilihan == "2":
-            os.system('cls')
-            p.display_stack()
-            choice = input("Sort by newest or oldest (1/2)\nX to back to main menu: ")
-            if choice == "1":
-                p.sort_by_newest()
-            elif choice == "2":
-                p.sort_by_oldest()
+        while True:
+            print("1. Play current song")
+            print("2. Play next song")
+            print("3. Play previous song")
+            print("4. Back to menu")
+            choice = int(input("Enter your choice: "))
+
+            if choice == 1:
+                p.play_songs()
+            elif choice == 2:
+                p.next_song()
+            elif choice == 3:
+                p.previous_song()
+            elif choice == 4:
+                print("Exiting...")
+                break
             else:
-                print("Masukkan input yang sesuai")
+                print("Invalid choice. Please try again.")
+
+    elif pilihan == "2":
+        os.system('cls')
+        p.display_stack()
+        choice = input("Sort by oldest or newest (1/2)\nX to go back to the main menu: ")
+        if choice == "1":
+            p.sort_by_oldest()
             
-        # Memanggil method untuk melihat daftar queue
-        # Menyortir urutan lagu berdasarkan terbaru dan terlama nya lagu ditambahkan
-        # Memainkan lagu berdasarkan preferensi urutan playlist; newest, oldest, shuffl
-       
+        elif choice == "2":
+            p.sort_by_newest()
+           
+        elif choice == 'X':
+            continue
+        else:
+            print("Masukkan input yang sesuai")
+
     elif pilihan == "3":
-        #Menambahkan sebuah lagu ke dalam stack, queue, dan deque
         os.system('cls')
         genre = input("Genre lagu: ")
         judul = input("Judul lagu: ")
-        artis = input("Nama artis: ") 
+        artis = input("Nama artis: ")
         while True:
             try:
                 durasi = int(input("Durasi waktu (dalam menit): "))
@@ -228,22 +240,18 @@ while True:
             else:
                 break
         p.add_song(genre, judul, artis, durasi)
-    
+
     elif pilihan == "4":
         os.system('cls')
         hapus_lagu = input("Judul lagu yang ingin dihapus: ")
-        # Menghapus spesifik lagu dari stack, queue, dan deque
-        
+
     elif pilihan == "5":
         os.system('cls')
         genre_lagu = input("Genre lagu yang ingin dicari: ")
         p.search_by_genre(genre_lagu)
-        # Mencari lagu berdasarkan genre
 
     elif pilihan == "6":
         break
-        # Keluar dari program
-    
+
     else:
         print("Input yang anda masukkan tidak sesuai, silahkan coba lagi")
-  
