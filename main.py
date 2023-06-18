ced7e8ff7613e4a890491157369da81e543bd134
Raihan Rifandi <112451785+raihanrifandi@users.noreply.github.com>
2023-06-18 10:50:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
+import os
 
+p  # Memanggil object class
+
+os.system('cls')
+file = open("header.txt", "r")
+print(file.read())
+
+while True:
+    print("\nGo-To Playlist")
+    print("[1] Melihat daftar lagu")
+    print("[2] Menambahkan lagu")
+    print("[3] Menghapus lagu")
+    print("[4] Mencari lagu")
+    print("[5] Keluar")
+    pilihan = input("Masukkan pilihan menu: ")
+    
+    if pilihan == "1":
+        os.system('cls')
+        # Memanggil method untuk melihat daftar queue
+        # Menyortir urutan lagu berdasarkan terbaru dan terlama nya lagu ditambahkan
+        # Memainkan lagu berdasarkan preferensi urutan playlist; newest, oldest, shuffl
+       
+    elif pilihan == "2":
+        os.system('cls')
+        # Menambahkan sebuah lagu ke dalam stack, queue, dan deque
+
+    elif pilihan == "3":
+        os.system('cls')
+        # Menghapus spesifik lagu dari stack, queue, dan deque
+        
+    elif pilihan == "4":
+        os.system('cls')
+        judul_lagu = input("Masukkan judul lagu: ")
+        # Mencari lagu berdasarkan genre, artis, atau judul
+                    
+    elif pilihan == "5":
+        break
+        # Keluar dari program
+    
+    else:
+        print("Input yang anda masukkan tidak sesuai, silahkan coba lagi")
